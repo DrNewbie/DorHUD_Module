b86cb4d5cfa7546e2bbdf68233c06bb39c77cc3d
Dr_Newbie <tf2baidonation@gmail.com>
2017-10-01 02:05:04 +0800

    Stockholm Syndrome ; 2 ; buff

--- (DorHud)Stockholm Syndrome/playerstandard.lua
+++ (DorHud)Stockholm Syndrome/playerstandard.lua
@@ -7,11 +7,11 @@ function PlayerStandard:_check_action_primary_attack(t, input)
 	local _res = _check_action_primary_attack(self, t, input)
 	if self._shooting and t > _StockholmSyndrome._delay then
 		math.randomseed(tostring(os.time()):reverse():sub(1, 6))
-		_StockholmSyndrome._delay = t + 10 + math.random()*10
+		_StockholmSyndrome._delay = t + math.random()*10
 		if math.random(1, 3) >= 2 then
 			local _local_pos = self._unit:position()
 			for u_key, u_data in pairs(managers.enemy:all_civilians()) do
-				if mvector3.distance(u_data.unit:position(), _local_pos) <= 1000 then
+				if mvector3.distance(u_data.unit:position(), _local_pos) <= 2500 then
 					u_data.unit:brain():on_intimidated(1, self._unit)
 				end			
 			end
