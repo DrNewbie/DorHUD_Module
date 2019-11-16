704c252b9da8ec85aa90f510e54fd91911b8e4a9
Dr_Newbie <tf2baidonation@gmail.com>
2019-11-16 13:01:47 +0800

    pd2_ace_inspire, 1.1
    it crashes when playing online because the damage classes aren't consistent in the game

--- (DorHud)pd2_ace_inspire/base.lua
+++ (DorHud)pd2_ace_inspire/base.lua
@@ -2,11 +2,65 @@ local module = DMod:new("pd2_ace_inspire", {
 	abbr = "pdinspire",
 	author = "Dr_Newbie",
 	description = "http://modwork.shop/25993",
-	version = "1"
+	version = "1.1"
 })
 
 module:hook_post_require("lib/units/beings/player/playermovement", "playermovement")
-
 module:hook_post_require("lib/units/beings/player/states/playerstandard", "playerstandard")
+
+module:add_menu_option(module:id() .. "_use_online", {
+	type = "boolean", text_id = "Enable in multiplayer mode", localize = false, default_value = false
+})
+
+local use_online = false
+
+local function toggle_inspire_state(enabled)
+	module:log(3, "OnNetworkDataRecv_CheckEnableInspire", "inspire enabled:", enabled)
+	module.enabled_in_lobby = enabled
+
+	local player_unit = managers.player and managers.player:player_unit()
+	if alive(player_unit) then
+		local movement_ext = player_unit:movement()
+		if movement_ext then
+			movement_ext:rally_skill_data().long_dis_revive = enabled
+		end
+	end
+end
+
+module:hook("OnModuleLoading", "OnModuleLoading_SetEnableInspire", function(module)
+	module.enabled_in_lobby = false
+
+	if Network:is_server() then
+		use_online = module:conf("use_online") -- can't change once started
+
+		module.enabled_in_lobby = true
+		module:hook("OnPeerAdded", "OnPeerAdded_CheckEnableInspire", function(peer)
+			peer:add_callback(peer.SYNC_DONE_CALLBACK, function(peer)
+				if use_online then
+					if not peer:is_local_user() and peer:has_dmf() then
+						DNet:send_to_peer(peer, "ModEvent", { module = module:id(), event = "EnableInspire" })
+					end
+				else
+					-- disabled even when not spawned
+					toggle_inspire_state(managers.network:session():amount_of_peers() == 1)
+				end
+			end)
+		end)
+
+		if not use_online then
+			module:hook("OnPeerRemoved", "OnPeerRemoved_CheckEnableInspire", function(peer)
+				toggle_inspire_state(managers.network:session():amount_of_peers() == 1)
+			end)
+		end
+	end
+end)
+
+module:hook("OnNetworkDataRecv", "OnNetworkDataRecv_CheckEnableInspire", "ModEvent", function(peer, data_type, data)
+	if not peer:is_server() or type(data) ~= "table" or data.event ~= "EnableInspire" then
+		return
+	end
+
+	toggle_inspire_state(data.value ~= false)
+end)
 
 return module
--- (DorHud)pd2_ace_inspire/playerstandard.lua
+++ (DorHud)pd2_ace_inspire/playerstandard.lua
@@ -1,47 +1,76 @@
-local module = ... or DMod:module('pd2_ace_inspire')
-
+local module = ... or D:module('pd2_ace_inspire')
 local PlayerStandard = module:hook_class("PlayerStandard")
 
-module:pre_hook(PlayerStandard, "_start_action_intimidate", function(self, _t_now)
-	if not self._intimidate_t or _t_now - self._intimidate_t > 0.5 then
-		if self._unit and self._unit:movement() then
-			local current_state_name = self._unit:movement():current_state_name()
-			if current_state_name ~= "arrested" and current_state_name ~= "bleed_out" and current_state_name ~= "fatal" and current_state_name ~= "incapacitated" then
-				local rally_skill_data = self._ext_movement:rally_skill_data()
-				if rally_skill_data and rally_skill_data.long_dis_revive_delay_t < _t_now then
-					local r_unit = nil
-					local criminals = managers.groupai:state():all_criminals()
-					for u_key, u_data in pairs(criminals) do
-						if u_data.unit and u_data.unit:movement() then
-							if mvector3.distance_sq(self._pos, u_data.unit:movement():m_pos()) < rally_skill_data.range_sq then
-								local cam_fwd = self._unit:camera():forward()
-								local vec = u_data.unit:movement():m_pos() - self._pos
-								local dis = mvector3.normalize(vec)
-								local max_angle = math.max(8, math.lerp(10, 30, dis / 1200))
-								local angle = vec:angle(cam_fwd)
-								if angle < max_angle or math.abs(max_angle-angle) < 10 then
-									r_unit = u_data.unit
-									if r_unit:interaction() and r_unit:interaction():active() then
-										r_unit:interaction():interact(self._unit)
-										r_unit:interaction():interact_interupt(self._unit)	
-									elseif r_unit:character_damage() and r_unit:character_damage():need_revive() and not r_unit:character_damage():arrested() then
-										r_unit:character_damage():revive(self._unit)
-									else
-										r_unit = nil
-									end
-									if r_unit then
-										self._ext_movement:rally_skill_data().long_dis_revive_delay_t = _t_now + rally_skill_data.long_dis_revive_cooldown_t
-										self._intimidate_t = _t_now + 0.5
-										self:say_line("e01x_sin")
-										self:_play_distance_interact_redirect(_t_now, "cmd_point")
-										break
-									end
-								end				
+module:hook(PlayerStandard, "_check_action_inspire", function(self, t)
+	if self._intimidate_t and t - self._intimidate_t <= 0.5 then
+		return
+	end
+
+	if not alive(self._unit) or not self._unit:movement() then
+		return
+	end
+
+	local current_state_name = self._unit:movement():current_state_name()
+	if current_state_name ~= "standard" then
+		return
+	end
+
+	local rally_skill_data = self._ext_movement:rally_skill_data()
+	if not rally_skill_data or not rally_skill_data.long_dis_revive or rally_skill_data.long_dis_revive_delay_t >= t then
+		return
+	end
+
+	local revived_unit = nil
+	local criminals = managers.groupai:state():all_criminals()
+	for u_key, u_data in pairs(criminals) do
+		local unit = u_data.unit
+		if alive(unit) and unit:movement() then
+			if mvector3.distance_sq(self._pos, unit:movement():m_pos()) < rally_skill_data.range_sq then
+				local cam_fwd = self._unit:camera():forward()
+				local vec = unit:movement():m_pos() - self._pos
+				local dis = mvector3.normalize(vec)
+				local max_angle = math.max(8, math.lerp(10, 30, dis / 1200))
+				local angle = vec:angle(cam_fwd)
+				if angle < max_angle or math.abs(max_angle - angle) < 10 then
+					local interaction_ext = unit:interaction()
+					if interaction_ext and interaction_ext:active() then -- ?
+						interaction_ext:interact(self._unit)
+						interaction_ext:interact_interupt(self._unit)
+						revived_unit = unit
+					else
+						local dmg_ext = unit:character_damage()
+						if dmg_ext then
+							local needs_revive = false
+							if dmg_ext.need_revive then
+								needs_revive = dmg_ext:need_revive() and not dmg_ext:arrested()
+							elseif dmg_ext.incapacitated then
+								needs_revive = dmg_ext:incapacitated()
+							end
+
+							if needs_revive then
+								dmg_ext:revive(self._unit)
+								revived_unit = unit
 							end
 						end
 					end
-				end
+					if revived_unit then
+						self._ext_movement:rally_skill_data().long_dis_revive_delay_t = t + rally_skill_data.long_dis_revive_cooldown_t
+						self._intimidate_t = t + 0.5
+						self:say_line("e01x_sin")
+						self:_play_distance_interact_redirect(t, "cmd_point")
+						break
+					end
+				end				
 			end
 		end
 	end
-end, false)
+
+	return revived_unit
+end, true)
+
+module:hook(PlayerStandard, "_start_action_intimidate", function(self, ...)
+	if self:_check_action_inspire(...) then
+		return true
+	end
+	return module:call_orig(PlayerStandard, "_start_action_intimidate", self, ...)
+end, true)
