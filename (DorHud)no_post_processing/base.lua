4652f645e083330db8f3055285af4990a65ce215
Dr_Newbie <tf2baidonation@gmail.com>
2020-01-05 06:57:39 +0800

    .

--- (DorHud)no_post_processing/base.lua
+++ (DorHud)no_post_processing/base.lua
@@ -1,8 +1,8 @@
 local module = DMod:new("no_post_processing", {
 	abbr = "nopostprocessing",
 	author = "Dr_Newbie",
-	description = "http://modwork.shop/",
-	version = "2"
+	description = "https://modworkshop.net/mod/26329/",
+	version = "1"
 })
 
 module:hook_post_require("core/lib/managers/managerbase/coremanagerbase", "coremanagerbase")
