b21daa396088b34a4a02be7192bec279931fe338
Dr_Newbie <tf2baidonation@gmail.com>
2022-01-29 22:15:46 +0800

    Update playerstandard.lua

--- (DorHud)test_place/playerstandard.lua
+++ (DorHud)test_place/playerstandard.lua
@@ -3,7 +3,7 @@ local MenuNodeGui = module:hook_class("PlayerStandard")
 
 module:post_hook(PlayerStandard, "update", function(self)
 	if self:shooting() then
-		log("pos:"..tostring(self._unit:position()))
-		log("rot:"..tostring(self._unit:rotation()))
+		--log("pos:"..tostring(self._unit:position()))
+		--log("rot:"..tostring(self._unit:rotation()))
 	end
 end, true)
